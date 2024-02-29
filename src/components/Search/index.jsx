5de8f598200126dbc65c490f00d232cac062d292
Vlad Solovey <connectwithvlad@gmail.com>
2024-02-29 20:56:16 +0300

    perf: use debounce for searching pizzas requests

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,17 +1,28 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
+import debounce from 'lodash.debounce';
 
 import { SearchContext } from '../../App';
 
 import classes from './index.module.scss';
 
 const Search = () => {
-  const { searchValue, setSearchValue } = useContext(SearchContext);
+  const [value, setValue] = useState('');
+  const { setSearchValue } = useContext(SearchContext);
+
+  const updateSearchValue = useCallback(debounce((value) => {
+    setSearchValue(value);
+  }, 300), []);
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  }
 
   return (
     <input
       className={classes.root}
-      value={searchValue}
-      onChange={(e) => setSearchValue(e.target.value)}
+      value={value}
+      onChange={onChangeInput}
       type="text"
       placeholder="Поиск пиццы ...."
     />
